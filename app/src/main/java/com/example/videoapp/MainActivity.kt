a74d74b5ef3e09ced43a7279bd65a5eb7a4d7317
Jade <nguyentrungngoc.051201@gmail.com>
2022-06-18 14:45:17 +0700

    finished all

--- app/src/main/java/com/example/videoapp/MainActivity.kt
+++ app/src/main/java/com/example/videoapp/MainActivity.kt
@@ -41,7 +41,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onCancelled(error: DatabaseError) {
-                TODO("Not yet implemented")
+
             }
 
         })
